b21e09f2fda29c71d89136936521d7bf6c5bc50d
khancon <ahnaf.khan1@gmail.com>
2023-12-28 14:29:41 -0500

    SideDrawer changes so HamBurger icon stays

--- src/components/NicNacAppBar.js
+++ src/components/NicNacAppBar.js
@@ -18,18 +18,6 @@ const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme }) => ({
   zIndex: theme.zIndex.drawer + 1,
-//   transition: theme.transitions.create(['width', 'margin'], {
-//     easing: theme.transitions.easing.sharp,
-//     duration: theme.transitions.duration.leavingScreen,
-//   }),
-//   ...(open && {
-//     marginLeft: drawerWidth,
-//     width: `calc(100% - ${drawerWidth}px)`,
-//     transition: theme.transitions.create(['width', 'margin'], {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   }),
 }));
 
 const NicNacAppBar = ({ handleDrawerOpen, toggleTheme, currentTheme }) => {
